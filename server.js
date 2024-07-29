e90645026dbde3ce0f4a31d35e25d99cb188be80
Steve <stvchrm@steves-mbp.lan>
2024-07-28 19:27:13 -0600

    create integrate routes with the server

--- server.js
+++ server.js
@@ -12,7 +12,29 @@ mongoose.connect('mongodb://localhost:27017/socialNetwork', {
 });
 
 mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB');const express = require('express');
+  const mongoose = require('mongoose');
+  const routes = require('./routes');
+  
+  const app = express();
+  const PORT = process.env.PORT || 3000;
+  
+  app.use(express.json());
+  app.use('/api', routes);
+  
+  mongoose.connect('mongodb://localhost:27017/socialNetwork', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+  mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+  
 });
 
 app.listen(PORT, () => {
